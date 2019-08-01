360119bacd2e3c03e1360195d33ec20efc96b51e
Konstantin Spirin <konstmsu@gmail.com>
2019-08-01 14:44:47 +0800

    Moving from TypeScript to Python

--- src/calculator.test.ts
+++
@@ -1,7 +0,0 @@
-import { add } from "./calculator";
-
-describe("calculator", () => {
-  it("add", () => {
-    expect(add(1, 2)).toEqual(3);
-  });
-});
--- src/playground.test.ts
+++
@@ -1,9 +0,0 @@
-import { randomInt, min, max } from "mathjs";
-import { range } from "./math";
-
-describe("playground", () => {
-  it("randomInt", () => {
-    const numbers = randomInt([1000], -2, 2.1) as number[];
-    expect(new Set(numbers)).toEqual(new Set([-2, -1, 0, 1, 2]));
-  });
-});
