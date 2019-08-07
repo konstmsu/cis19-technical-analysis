75b4dc7671f817704b49e784f7598a050d925042
Konstantin Spirin <konstmsu@gmail.com>
2019-08-07 23:00:01 +0800

    Refactoring and test fix

--- app/evaluate.py
+++ app/evaluate.py
@@ -2,59 +2,59 @@ import json
 import logging
 import requests
 
-from flask import request, current_app, Blueprint
+from flask import request, current_app, Blueprint, jsonify
 
-bp = Blueprint("evaluate", __name__,)
-logger = logging.getLogger(__name__)
+bp = Blueprint("evaluate", __name__)
 
 
-@bp.route('/test')
+@bp.route("/test")
 def test_route():
     return "It Works!"
 
 
-@bp.route('/evaluate', methods=['POST'])
+@bp.route("/evaluate", methods=["POST"])
 def evaluate():
     data = request.get_json()
     current_app.logger.warn(data)
     teamUrl = data["teamUrl"]
     callbackUrl = data["callbackUrl"]
     runId = data["runId"]
-    logging.info('teamUrl:' + teamUrl + '- callbackUrl:' +
-                 callbackUrl + '- runId:' + runId)
+    current_app.logger.info(
+        f"teamUrl: {teamUrl}, callbackUrl: {callbackUrl}, runId: {runId}"
+    )
     result = execute_team_solution(teamUrl)
     returnMessage = calculate_score(result, runId)
 
-    urlSubstring = callbackUrl.split('://')
-    secureUrl = urlSubstring[0] + \
-        '://cisadmin:soltandpepper@' + urlSubstring[1]
+    urlSubstring = callbackUrl.split("://")
+    secureUrl = urlSubstring[0] + "://cisadmin:soltandpepper@" + urlSubstring[1]
     requests.post(secureUrl, data=returnMessage)
+    return jsonify({"evaluate_status": "complete"})
 
 
 def execute_team_solution(teamUrl):
     testData = {}
-    testData['input'] = 2
+    testData["input"] = 2
     json_testData = json.dumps(testData)
-    url = teamUrl + '/challenge'
-    response = requests.post(url, data=json_testData)
-    logger.info('teamUrl: ' + teamUrl + 'team result: ' + response.content)
+    url = teamUrl + "/challenge"
+    response_content = str(requests.post(url, data=json_testData).content, "utf-8")
+    current_app.logger.info("teamUrl: " + teamUrl + "team result: " + response_content)
 
-    return response.content
+    return response_content
 
 
 def calculate_score(result, runId):
     expectedResult = 4
     marksScrored = 0
-    message = ''
+    message = ""
     if result == expectedResult:
         marksScrored = 100
-        message = 'Your score ' + marksScrored
+        message = "Your score " + marksScrored
     else:
         message = "Failed"
 
     responseMessage = {}
-    responseMessage['runId'] = runId
-    responseMessage['score'] = marksScrored
-    responseMessage['message'] = message
+    responseMessage["runId"] = runId
+    responseMessage["score"] = marksScrored
+    responseMessage["message"] = message
 
     return json.dumps(responseMessage)
--- test/test_app.py
+++ test/test_app.py
@@ -1,5 +1,6 @@
 import os
 import tempfile
+import responses
 
 import pytest
 from app import create_app
@@ -8,17 +9,39 @@ from app import create_app
 @pytest.fixture
 def client():
     app = create_app()
-    app.config['TESTING'] = True
+    app.config["TESTING"] = True
 
     with app.test_client() as client:
         yield client
 
 
-def test_root_redirect(client):
-    rv = client.get('/')
-    assert rv.status_code == 302
-
-
 def test_instructions(client):
-    rv = client.get('/instructions')
-    assert b'Build trading strategy' in rv.data
+    for url in ["/", "/instructions"]:
+        rv = client.get(url, follow_redirects=True)
+        assert b"Build trading strategy" in rv.data
+
+
+@responses.activate
+def test_evaluate(client):
+    responses.add(
+        responses.POST,
+        "http://solver/challenge",
+        json={"error": "not found"},
+        status=404,
+    )
+
+    responses.add(
+        responses.POST,
+        "http://cisadmin:soltandpepper@codeit-suisse/result",
+        json={"error": "not found"},
+        status=404,
+    )
+
+    client.post(
+        "/evaluate",
+        json={
+            "teamUrl": "http://solver",
+            "callbackUrl": "http://codeit-suisse/result",
+            "runId": "142",
+        },
+    )
